b458da3740ccc3af573537a12aa32890b0d6c1c8
Aleksey Ilyin <alksily@outlook.com>
2016-07-17 15:02:50 +0300

    fix debug event on shutdown

--- index.php
+++ index.php
@@ -8,16 +8,16 @@ require_once("bootstrap.php");
 
 $app = \Orchid\App::getInstance();
 
-// add tasks outputting time and memory in the response header
-$app->event()->on("shutdown", function () use ($app) {
-	if ($app->isDebug()) {
+if ($app->isDebug()) {
+	// add tasks outputting time and memory in the response header
+	$app->event()->on("shutdown", function () use ($app) {
 		$app->response()
 		    ->setHeaderCacheControl(284, 0, 0)
 		    ->setHeaderExpires(DateTime::createFromFormat(DATE_RFC2822, "Sat, 01 Jan 00 00:00:00 +0000"))
 		    ->setHeader("X-Time", round(microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"], 7) . "ms")
 		    ->setHeader("X-Memory", Str::convertSize(memory_get_usage()));
-	}
-});
+	});
+}
 
 // запускаем приложение
 $app->run();
