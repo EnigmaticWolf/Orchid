84f92d82858fc6edc2c205a4a064493dcca38752
Aleksey Ilyin <alksily@outlook.com>
2016-10-09 17:36:14 +0300

    stop implementation of the PSR-7

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 spl_autoload_register(function ($class) {
     $class_path = __DIR__ . '/src/' . str_replace(['\\', '_'], '/', $class) . '.php';
+    $class_path = str_replace('AEngine/Orchid/', '', $class_path);
 
     if (file_exists($class_path)) {
         require_once($class_path);
